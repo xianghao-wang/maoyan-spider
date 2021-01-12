2ddcd486a8f217c8e7171b5a9c869bb63a0e9e3a
Xianghao Wang <xianghaowang2001@outlook.com>
2021-01-12 13:56:47 +0800

    .gitignore add tmps/

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import os
 from app import *
 
+# 存储test输出洁面的文件夹
+if os.path.exists('tmps'):
+    os.mkdir('tmps')
+
 # Test1 : 获取一页
 result = get_one_page(os.getenv('URL') + '?offset=0')
 with open('tmps/test1.html', 'w') as f:
